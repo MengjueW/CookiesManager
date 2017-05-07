96c97519331d1f467faf84f6d40e76c39249b699
MengjueW <mw856@cornell.edu>
2017-05-07 13:14:53 -0400

    finish notification

--- js/background.js
+++ js/background.js
@@ -26,6 +26,45 @@ function focusOrCreateTab(url) {
 	});
 }
 
+// Notification
+function show() {
+  var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
+  var hour = time[1] % 12 || 12;               // The prettyprinted hour.
+  var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
+  new Notification(hour + time[2] + ' ' + period, {
+    icon: './icon.png',
+    body: 'Time to manage your cookies.'
+  });
+}
+
+// Conditionally initialize the options.
+if (!localStorage.isInitialized) {
+  localStorage.isActivated = true;   // The display activation.
+  localStorage.frequency = 1;        // The display frequency, in minutes.
+  localStorage.isInitialized = true; // The option initialization.
+}
+
+console.log(window.Notification);
+// Test for notification support.
+if (window.Notification) {
+  // While activated, show notifications at the display frequency.
+  if (JSON.parse(localStorage.isActivated)) { show(); }
+
+  var interval = 0; // The display interval, in minutes.
+
+  setInterval(function() {
+    interval++;
+
+    if (
+      JSON.parse(localStorage.isActivated) &&
+        localStorage.frequency <= interval
+    ) {
+      show();
+      interval = 0;
+    }
+  }, 60000 * 24);
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 	var manager_url = chrome.extension.getURL("../background.html");
 	focusOrCreateTab(manager_url);
